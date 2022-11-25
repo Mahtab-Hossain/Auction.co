701d6fe988e5e06a4f8302f596643b5ad39760a2
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-11-26 04:45:21 +0600

    added second route in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
+});
+Route::get('/logout',function(){
+    Session::forget('user');
+    return redirecct('login');
 });

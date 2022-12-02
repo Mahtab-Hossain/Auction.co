0050cfb734bfbf3165670e61429c19009419c263
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-12-02 23:57:52 +0600

    bug with syntax error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     //
+    function login(Request $req){
+        $user= User::where(['email'=>$req->'email'])->first();
+    }
 }

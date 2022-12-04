7f495f291fc4563895e56987dc02c29fc5dcd97f
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-12-04 23:47:46 +0600

    modified usercontroll

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,13 +12,12 @@ class UserController extends Controller
     function login(Request $req)
     {
         $user= User::where(['email'=>$req->email])->first();
-        if(!$user || !Hash::check($req->password,$user->password))
-        {
-            return "Username or password does not match";
-
+        return $user->password;
+        if(!$user || !Hash::check($req->password , $user->password)){
+            return "username or password does not match";
         }
         else{
-            $req->session()->put('user', $user);
+            $req->session()->put('user',$user);
             return redirect('/');
         }
     }
